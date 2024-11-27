dfee7b6025610dc93b265f5cc03ef8b34dd73cfe
Thomas A Caswell <tcaswell@bnl.gov>
2024-11-27 12:50:15 -0500

    ENH: add check-point saving and cli args

--- sims/hello_world/runner.py
+++ sims/hello_world/runner.py
@@ -9,28 +9,71 @@ from bad_tools.config import AnalyzerConfig, DetectorConfig, SimConfig, SourceCo
 from bad_tools.xrt.endstation import Endstation
 
 
-def scan(
+def scan_to_file(
     bl: Endstation,
     start: float,
     stop: float,
     delta: float,
+    *,
+    writer,
 ):
+    cache_rate = writer.send(None)
+    writer.send(bl)
     start, stop, delta = np.deg2rad([start, stop, delta])
-    tths = np.linspace(start, stop, int((stop - start) / delta))
-    good = {f"screen{screen:02d}": [] for screen in range(bl.analyzer.N)}
-    bad = {f"screen{screen:02d}": [] for screen in range(bl.analyzer.N)}
-    for tth in tqdm.tqdm(tths):
-        bl.set_arm(tth)
-        images, *rest = bl.get_frames()
-        for k, v in images.items():
-            good[k].append(v["good"])
-            bad[k].append(v["bad"])
+    N = int((stop - start) / delta)
+    tths = np.linspace(start, stop, N)
+    for batch in tqdm.tqdm(range(len(tths) // N + 1)):
+        batch_tth = tths[batch * cache_rate : (batch + 1) * cache_rate]
+        good = {f"screen{screen:02d}": [] for screen in range(bl.analyzer.N)}
+        # bad = {f"screen{screen:02d}": [] for screen in range(bl.analyzer.N)}
 
-    return (
-        {k: np.asarray(v) for k, v in good.items()},
-        {k: np.asarray(v) for k, v in bad.items()},
-        tths,
-    )
+        for tth in tqdm.tqdm(batch_tth):
+            bl.set_arm(tth)
+            images, *rest = bl.get_frames()
+            for k, v in images.items():
+                good[k].append(v["good"])
+                # bad[k].append(v["bad"])
+        writer.send((batch_tth, to_block(good)))
+    writer.close()
+
+
+def dump_coro(fname, cache_rate, *, tlg="sim"):
+    print("a")
+    bl = yield cache_rate
+    print(f"{bl=}")
+    with h5py.File(fname, "x") as f:
+        g = f.create_group(tlg)
+        for fld in fields(bl):
+            if is_dataclass(fld.type):
+                cfg_g = g.create_group(f"{fld.name}_config")
+                cfg_g.attrs.update(asdict(getattr(bl, fld.name)))
+        g.create_dataset(
+            "block",
+            chunks=(cache_rate, bl.analyzer.N, 1, bl.detector.transverse_size),
+            maxshape=(None, bl.analyzer.N, 1, bl.detector.transverse_size),
+            shape=(0, bl.analyzer.N, 1, bl.detector.transverse_size),
+            shuffle=True,
+            compression="gzip",
+        )
+        g.create_dataset(
+            "tth",
+            chunks=(cache_rate,),
+            maxshape=(None,),
+            shape=(0,),
+            shuffle=True,
+            compression="gzip",
+        )
+    print(f"{fname=}")
+    while True:
+        payload = yield
+        with h5py.File(fname, "a") as f:
+            g = f[tlg]
+            for name, data in zip(("tth", "block"), payload, strict=True):
+                ds = g[name]
+                cur_len = ds.shape[0]
+                print(f"{data=}")
+                ds.resize(cur_len + data.shape[0], axis=0)
+                ds[cur_len:] = data
 
 
 def dump(fname, data, tth, bl, *, tlg="sim"):
@@ -61,6 +104,15 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Run some sims.")
     parser.add_argument("fout", help="File to write results to.", type=Path)
+    parser.add_argument("start", help="start angle in deg", type=float)
+    parser.add_argument("stop", help="stop angle in deg", type=float)
+    parser.add_argument("delta", help="angle steps in deg", type=float)
+    parser.add_argument(
+        "--cache-rate",
+        help="flush to disk every this number of angles",
+        type=int,
+        default=10_000,
+    )
     args = parser.parse_args()
 
     args.fout.parent.mkdir(parents=True, exist_ok=True)
@@ -88,6 +140,5 @@ if __name__ == "__main__":
     )
 
     bl = Endstation.from_configs(config, source_config, detector_config, sim_config)
-
-    good, bad, tths = scan(bl, 1, 3, 1e-2)
-    dump(args.fout, good, tths, bl)
+    writer = dump_coro(args.fout, args.cache_rate)
+    scan_to_file(bl, args.start, args.stop, args.delta, writer=writer)
