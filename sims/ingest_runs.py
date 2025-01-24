7a8da30f8ad8714457a84ed1599fef92413d5118
Thomas A Caswell <tcaswell@bnl.gov>
2025-01-24 15:01:45 -0500

    WIP: more work on analysis of runs

--- sims/ingest_runs.py
+++ sims/ingest_runs.py
@@ -12,9 +12,9 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from hello_world import reduce
 from multianalyzer import Result
 from scipy.interpolate import splrep, sproot
+from sims.hello_world import reduce
 
 # %%
 os.environ["PYOPENCL_CTX"] = "1"
@@ -31,7 +31,7 @@ reference_pattern = pd.read_csv(
 # %%
 
 # config = reduce.load_all_config(Path('/nsls2/data3/projects/next_iiia_hrd/xrt_output/'))
-config = reduce.load_all_config(Path("/mnt/scratch/hrd/sims/xrt_output/"))
+config = reduce.load_all_config(Path("/mnt/scratch/hrd/sims/xrt_output/20250123"))
 
 # %%
 df = pd.DataFrame(
@@ -44,7 +44,7 @@ df = pd.DataFrame(
         for k, v in config.items()
     }
 ).T
-df["job"] = [_.name[:-4] for _ in df.index]
+df["job"] = [_.name.partition("-")[0] for _ in df.index]
 
 
 # %%
@@ -122,17 +122,37 @@ def peak_fwha(tth, normed, limits: tuple[float, float], k: int = 10):
 
 # %%
 
-for _, g in df.groupby("job"):
-    fig, ax = plt.subplots()
+
+def plot_df(g: pd.DataFrame, title=None):
+    fig, ax = plt.subplots(layout="constrained")
+    if title:
+        fig.suptitle(title)
     label_keys = find_varied_config([results[k][1] for k in g.index])
+    unit_convert = defaultdict(lambda: (lambda x: x, ""))
+    unit_convert.update(
+        {
+            ("source", "h_div"): (lambda x: np.deg2rad(x) * 1e3, "mrad"),
+            ("source", "v_div"): (lambda x: np.deg2rad(x) * 1e3, "mrad"),
+        }
+    )
+    g = g.sort_values(by=[".".join(lk) for lk in label_keys])
     for k in g.index:
         res, config, _ = results[k]
         label = " ".join(
-            f"{sec}.{parm}={getattr(getattr(config, sec), parm):.02g}"
+            f"{sec}.{parm}={unit_convert[(sec, parm)][0](getattr(getattr(config, sec), parm)):.3g} {unit_convert[(sec, parm)][1]}"
             for sec, parm in label_keys
         )
         plot_reduced(res, ax=ax, scale_to_max=True, label=label, alpha=0.5)
-    plot_ref(reference_pattern, ax, scalex=False)
+    plot_ref(reference_pattern, ax, scalex=False, linestyle=":")
     ax.set_xlim(7.5, 8.3)
     ax.legend()
+
+
+plot_df(df[df["source.v_div"] == 0], "with vdiv=0")
+plot_df(df[df["source.h_div"] == 0], "with hdiv=0")
+# %%
+
+for _, g in df.groupby("job"):
+    plot_df(g)
+
 plt.show()
