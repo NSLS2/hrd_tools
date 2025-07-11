64b47e7e39530cd986d65808778a77378c8498e7
Thomas A Caswell <tcaswell@bnl.gov>
2025-07-11 07:22:12 -0400

    WIP: more progress on computing angles

--- examples/effective_angle.py
+++ examples/effective_angle.py
@@ -9,7 +9,7 @@ import xrt.backends.raycing.materials as rmats
 def angle(n1, v1):
     return np.arccos(
         np.vecdot(n1, v1)
-        / (np.linalg.vector_norm(n1, axis=2) * np.linalg.vector_norm(v1, axis=2))
+        / (np.linalg.vector_norm(n1) * np.linalg.vector_norm(v1, axis=1))
     )
 
 
@@ -32,32 +32,32 @@ darwin = crystal.get_Darwin_width(E)
 phi = np.deg2rad(np.linspace(0, 1, 1024))
 
 # %%
-
-fig, ax = plt.subplots(layout="constrained")
-
-for arm in np.deg2rad([15, 45, 60, 90]):
-    n1 = to_norm(arm, 0)
-    v = to_norm(arm, phi).T
-
-    ax.plot(
-        np.rad2deg(phi),
-        np.rad2deg((angle(n1, v))),
-        label=f"$2\\theta = {np.rad2deg(arm):.1f}\\degree$",
-    )
-ax.set_title(f"at {E / 1_000}kEv")
-ax.annotate(
-    f"Si 111 Darwin width\n({np.rad2deg(darwin):.2g}$\\degree$)",
-    (0, np.rad2deg(darwin)),
-    xytext=(5, 5),
-    textcoords="offset pixels",
-)
-ax.axhline(np.rad2deg(darwin))
-ax.set_xlim(0, 1)
-# ax.set_ylim(0, np.rad2deg(darwin) * 3)
-ax.set_xlabel(r"$\phi$ (deg)")
-ax.set_ylabel(r"$\theta_{bragg} - 2\theta_{eff}$ (deg)")
-ax.legend()
-
+#
+# fig, ax = plt.subplots(layout="constrained")
+#
+# for arm in np.deg2rad([15, 45, 60, 90]):
+#     n1 = to_norm(arm, 0).T
+#     v = to_norm(arm, phi).T
+#
+#     ax.plot(
+#         np.rad2deg(phi),
+#         np.rad2deg(angle(n1[:, np.newaxis,:], v[np.newaxis, :, :)),
+#         label=f"$2\\theta = {np.rad2deg(arm):.1f}\\degree$",
+#     )
+# ax.set_title(f"at {E / 1_000}kEv")
+# ax.annotate(
+#     f"Si 111 Darwin width\n({np.rad2deg(darwin):.2g}$\\degree$)",
+#     (0, np.rad2deg(darwin)),
+#     xytext=(5, 5),
+#     textcoords="offset pixels",
+# )
+# ax.axhline(np.rad2deg(darwin))
+# ax.set_xlim(0, 1)
+# # ax.set_ylim(0, np.rad2deg(darwin) * 3)
+# ax.set_xlabel(r"$\phi$ (deg)")
+# ax.set_ylabel(r"$\theta_{bragg} - 2\theta_{eff}$ (deg)")
+# ax.legend()
+#
 # %%
 
 phi = np.deg2rad(np.linspace(0, 1, 1024))
@@ -66,7 +66,7 @@ arm_th = np.linspace(-1, 6, 5120)
 for arm in np.deg2rad(arm_th):
     n1 = to_norm(arm - bragg, 0)
     v = to_norm(arm, phi).T
-    out.append(np.rad2deg(0*bragg - (angle(n1, v))))
+    out.append(np.rad2deg(0 * bragg - (angle(n1, v))))
 
 angle_deviation = np.asarray(out)
 
@@ -74,7 +74,7 @@ angle_deviation = np.asarray(out)
 # 1m sample -> crystal
 d = 1
 # 320*75um for medipix4
-detector_width = 320*75*10e-6
+detector_width = 320 * 75 * 10e-6
 
 # 256*55um for medipix3
 # detector_width = 256 * 55 * 10e-6
@@ -99,12 +99,12 @@ ax.plot(
     label="max $\phi$ from Darwin",
 )
 ax.legend()
-ax.set_title(f'sample-to-detector {d}m, detector_width {detector_width*1000:.1f}mm')
+ax.set_title(f"sample-to-detector {d}m, detector_width {detector_width * 1000:.1f}mm")
 
 # %%
 plt.figure()
 plt.imshow(
-    #np.abs(angle_deviation) < darwin,
+    # np.abs(angle_deviation) < darwin,
     bragg - angle_deviation,
     aspect="auto",
     extent=(
@@ -123,10 +123,107 @@ plt.colorbar()
 fig, ax = plt.subplots(layout="constrained")
 
 ring = 25
-arm = np.linspace(ring-.5, ring+.5, 512)
-phi = np.deg2rad(np.linspace(-.1, .1, 1024))
+arm = np.linspace(ring - 0.01, ring + 0.01, 512)
+phi = np.linspace(-0.10, 0.10, 256)
 n1 = to_norm(np.deg2rad(arm), 0).T
 v = to_norm(np.deg2rad(ring), phi).T
 
+angles = np.arccos(np.vecdot(v[np.newaxis, :, :], n1[:, np.newaxis, :]))
+# angles = np.vecdot(v[np.newaxis, :, :], n1[:, np.newaxis, :])
 
-ax.imshow(angle(n1, v))
+plt.imshow(
+    angles,
+    aspect="auto",
+    extent=[np.min(phi), np.max(phi), np.min(ring - arm), np.max(ring - arm)],
+)
+
+# %%
+
+# Demonstration of the full ring passing note that the arm angle to cover the
+# full range is smaller at small angles so at a fixed phi range look "flatter"
+# for a fixed movement of the arm despite having higher curvature
+
+
+def plot_ring_pass(ring, ax):
+    arm = np.linspace(0 * ring - 0.02, ring + 0.01, 512 * 2)
+    phi = np.linspace(-90, 90, 256 * 2)
+    n1 = to_norm(np.deg2rad(arm), 0).T
+    v = to_norm(np.deg2rad(ring), np.deg2rad(phi)).T
+
+    ax.set_title(f"{ring=}")
+    # angles = np.arccos(np.vecdot(v[np.newaxis, :, :], n1[:, np.newaxis, :]))
+    return ax.imshow(
+        np.abs(
+            np.atan(n1[:, 0] / n1[:, 2])[:, np.newaxis]
+            - np.atan(v[:, 0] / v[:, 2])[np.newaxis, :]
+        )
+        < darwin * 100,
+        origin="lower",
+        extent=[np.min(phi), np.max(phi), ring - np.min(arm), ring - np.max(arm)],
+        aspect="auto",
+    )
+
+
+fig, ax_lst = plt.subplots(3, layout="constrained")
+for ring, ax in zip([1, 25, 45], ax_lst):
+    plot_ring_pass(ring, ax)
+# %%
+
+# This shows what we would expect to see on a fixed sized detector at several
+# angles.  Countering the affect above, the restriction of the accssible phi at
+# higher angles makes them look flatavailable phqter
+
+
+def effoctive_solid_angle(theta, d, detector_width):
+    # radius of DS ring at detector
+    R = d * np.abs(np.sin(theta))
+    return np.arctan2(detector_width / 2, R)
+
+
+def plot_ring_pass(ring, ax, d_width, d_R):
+    arm = np.linspace(ring - 0.1, ring + 0.01, 512 * 2)
+    dphi = effoctive_solid_angle(np.deg2rad(np.mean(arm)), d_R, d_width)
+    phi = np.rad2deg(np.linspace(-dphi, dphi, 256 * 2))
+    n1 = to_norm(np.deg2rad(arm), 0).T
+    v = to_norm(np.deg2rad(ring), np.deg2rad(phi)).T
+
+    ax.set_title(f"{ring=}")
+    angles = np.arccos(np.vecdot(v[np.newaxis, :, :], n1[:, np.newaxis, :]))
+    return ax.imshow(
+        np.abs(
+            np.atan(n1[:, 0] / n1[:, 2])[:, np.newaxis]
+            - np.atan(v[:, 0] / v[:, 2])[np.newaxis, :]
+        )
+        < darwin,
+        origin="lower",
+        extent=[np.min(phi), np.max(phi), ring - np.min(arm), ring - np.max(arm)],
+        aspect="auto",
+    )
+    # ax.plot(v[:, 0])
+
+
+dets = {
+    # 320*75um for medipix4
+    "medipix4": 320 * 75 * 1e-6,
+    # 256*55um for timepix4
+    "timepix4": 512 * 55 * 1e-6,
+    # 256*55um for medipix3, timepix3
+    "medipix3": 256 * 55 * 1e-6,
+    # 1028*75um for eiger2
+    "eiger2": 1028 * 75 * 1e-6,
+}
+
+fig, ax_lst = plt.subplots(3, layout="constrained", sharex=True)
+for ring, ax in zip([75, 45, 15], ax_lst):
+    plot_ring_pass(ring, ax, dets["medipix4"], 1)
+
+# %%
+
+
+def plotz(ax_lst, vec):
+    for ax, data in zip(ax_lst, vec.T):
+        ax.plot(data)
+
+
+fig, ax_lst = plt.subplots(3)
+plotz(ax_lst, n1)
